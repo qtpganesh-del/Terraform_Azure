1ef1ff9dde2f27595d6c905b73de94122b2dd919
qtpganesh-del <qtpganesh@gmail.com>
2026-03-23 09:36:05 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-tf-backend"
-    storage_account_name = "tfstatebackend123"
+    storage_account_name = "tfstatebackend1808"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
